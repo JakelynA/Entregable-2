c2dc4b9827a4bd89460307b02673ac03e3008691
Jakelyn <1427468@senati.pe>
2024-05-28 21:36:11 -0500

    entregable-2

--- app/Models/Inquilino.php
+++ app/Models/Inquilino.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class inquilino extends Model
 {
     use HasFactory;
+    
     protected $table ='inquilino';
  
     protected $fillable = ['nombre','email'];
